75c4adf928227da5b7ade4cdf82c6fce7e24191a
joshi88 <amitkjoshi88@gmail.com>
2024-07-08 09:50:11 +0530

    Update lcg.js

--- lcg.js
+++ lcg.js
@@ -1,11 +1,25 @@
   // Fetch all menu links.
-  $menu_links = \Drupal::entityTypeManager()
-    ->getStorage('menu_link_content')
-    ->loadMultiple();
+  $menu_names = ['main-menu', 'footer-menu']; // Add your menu names here.
+  foreach ($menu_names as $menu_name) {
+    $menu_tree = \Drupal::menuTree();
+    $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name);
 
-  foreach ($menu_links as $menu_link) {
-    if ($menu_link->hasTranslation($language->getId())) {
-      $translated_menu_link = $menu_link->getTranslation($language->getId());
-      $output .= generate_url_entry($translated_menu_link->toUrl()->setAbsolute());
+    // Build the tree according to the parameters.
+    $tree = $menu_tree->load($menu_name, $parameters);
+
+    // Transform the tree using the default manipulators.
+    $manipulators = [
+      ['callable' => 'menu.default_tree_manipulators:checkAccess'],
+      ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
+    ];
+    $tree = $menu_tree->transform($tree, $manipulators);
+
+    foreach ($tree as $element) {
+      if ($element->access->isAllowed()) {
+        $link = $element->link;
+        if ($link->getUrlObject()->isRouted()) {
+          $output .= generate_url_entry($link->getUrlObject()->setAbsolute());
+        }
+      }
     }
   }
