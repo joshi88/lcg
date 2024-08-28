e64831ed12420c3cca0e60b645cd2591e66cda4e
joshi88 <amitkjoshi88@gmail.com>
2024-08-28 09:26:54 +0530

    Update lcg.js

--- lcg.js
+++ lcg.js
@@ -1,38 +1,49 @@
-$menu_link_tree = \Drupal::service('menu.link_tree');
-$menu_link_manager = \Drupal::service('plugin.manager.menu.link');
-$renderer = \Drupal::service('renderer');
-$home_url = Url::fromRoute('<front>')->toString();
-// Define the menu name you want to work with.
-$menu_name = 'top-menu';
-
-// Create menu tree parameters.
-$parameters = new MenuTreeParameters();
-$parameters->setMaxDepth(2); // Set the desired depth.
-
-// Load the menu tree.
-$tree = $menu_link_tree->load($menu_name, $parameters);
-
-// Transform the tree using manipulators you can find in the menu.link_tree service.
-$manipulators = [
-  ['callable' => 'menu.default_tree_manipulators:checkAccess'],
-  ['callable' => 'domain_menu_access.default_tree_manipulators:checkDomain'],
-  ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
-];
-$tree = $menu_link_tree->transform($tree, $manipulators);
-
-// Traverse and manipulate the tree.
-foreach ($tree as $key => $element) {
-  $link = $element->link;
-  $subtree = $element->subtree;
-  if($home_url != $link->getUrlObject()->toString()){
-    $output .= $this->generateUrlEntry($link->getUrlObject()->setAbsolute(TRUE));
-  }
-
-  if (!empty($subtree)) {
-    foreach ($subtree as $sub_element) {
-      if($home_url != $sub_element->link->getUrlObject()->toString()){
-        $output .= $this->generateUrlEntry($sub_element->link->getUrlObject()->setAbsolute(TRUE));
-      }
-    }
-  }
+.views-exposed-widgets .bef-link a:contains("Any") {
+    display: none;
 }
+(function ($) {
+  $(document).ready(function () {
+    // Initially hide the "False" link
+    var falseLink = $('.views-exposed-widgets .bef-link a:contains("False")');
+    var trueLink = $('.views-exposed-widgets .bef-link a:contains("True")');
+    
+    falseLink.hide();
+    
+    // When the "True" link is clicked
+    trueLink.click(function (e) {
+      e.preventDefault();
+      
+      // If True is active, show False and hide True
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active').hide();
+        falseLink.addClass('active').show();
+        $('input[name="combine"]').val('0');  // Set filter value to false
+      } else {
+        $(this).addClass('active');
+        $('input[name="combine"]').val('1');  // Set filter value to true
+      }
+      
+      // Trigger the AJAX submission
+      $('.views-exposed-form').trigger('submit');
+    });
+
+    // When the "False" link is clicked
+    falseLink.click(function (e) {
+      e.preventDefault();
+      
+      // If False is active, show True and hide False
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active').hide();
+        trueLink.addClass('active').show();
+        $('input[name="combine"]').val('1');  // Set filter value to true
+      } else {
+        $(this).addClass('active');
+        $('input[name="combine"]').val('0');  // Set filter value to false
+      }
+      
+      // Trigger the AJAX submission
+      $('.views-exposed-form').trigger('submit');
+    });
+  });
+})(jQuery);
+
